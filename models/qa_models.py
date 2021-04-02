57e7dec4c2018c2cd4dc89177224813fbc78cad5
Eliav Mor <moreliav@rack-gamir-g02.cs.tau.ac.il>
2021-04-02 15:45:47 +0300

    update qa_models.py

--- models/qa_models.py
+++ models/qa_models.py
@@ -200,7 +200,8 @@ if __name__ == "__main__":
         train_model(config)
     else:
         #test_files = ["csv/animals_dont_have_horns_questions.csv", "csv/animals_dont_have_hair_questions.csv", "csv/animals_dont_have_fur_questions.csv", "csv/animals_dont_have_feathers_questions.csv", "csv/animals_dont_have_a_beak_questions.csv", "csv/animals_dont_have_wings_questions.csv", "csv/animals_dont_have_fins_questions.csv", "csv/animals_dont_have_scales_questions.csv"]
-        test_files = ["csv/animals_dont_have_a_beak_questions.csv", "csv/animals_dont_have_wings_questions.csv", "csv/animals_dont_have_fins_questions.csv", "csv/animals_dont_have_scales_questions.csv", "csv/conceptnet_train_no_animals.csv", "conceptnet_dev.csv"]
+        #test_files = ["csv/animals_dont_have_a_beak_questions.csv", "csv/animals_dont_have_wings_questions.csv", "csv/animals_dont_have_fins_questions.csv", "csv/animals_dont_have_scales_questions.csv", "csv/conceptnet_train_no_animals.csv", "conceptnet_dev.csv"]
+        test_files = ["csv/animals_cant_fly_questions.csv", "csv/animals_drink_questions.csv"]
         for f in test_files:
             config["test_data"] = f
             test_model(config, f.replace(".csv", "_result.csv"))
